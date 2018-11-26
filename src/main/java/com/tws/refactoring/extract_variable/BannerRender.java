1d00ea10ea26b531bc46aaf36c979c50d427fc48
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 22:40:22 +0800

    refactoring change indexOf to contains for matchBrowser

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -3,7 +3,7 @@ package com.tws.refactoring.extract_variable;
 public class BannerRender {
     void renderBanner(String platform, String browser) {
         boolean matchPlatform = platform.toUpperCase().contains("MAC");
-        boolean matchBrowser = (browser.toUpperCase().indexOf("IE") > -1);
+        boolean matchBrowser = browser.toUpperCase().contains("IE");
         if (matchPlatform && matchBrowser) {
             // do something
             System.out.print("pass the if condition");
