5ecb3a61f55a0b926e028d4438e162ec9b9f6a10
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 23:02:53 +0800

    refactoring extract shippingCostRate variable

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -2,14 +2,15 @@ package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
     private final double maxShippingCost = 100.0;
+    private final double shippingCostRate = 0.1;
 
     double getPrice(int quantity, int itemPrice) {
         double basePrice = quantity * itemPrice;
 
         double discountedQuantity = Math.max(0, quantity - 500);
         double discount = discountedQuantity * itemPrice * 0.05;
-        
-        double shippingCost=Math.min(basePrice * 0.1, maxShippingCost);
+
+        double shippingCost=Math.min(basePrice * shippingCostRate, maxShippingCost);
 
         return basePrice - discount + shippingCost;
     }
