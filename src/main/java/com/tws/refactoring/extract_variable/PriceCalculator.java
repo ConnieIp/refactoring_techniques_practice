002a2d827b6a754425393df7619ffb78becad027
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 23:02:04 +0800

    refactoring extract discountedQuantity variable

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -4,9 +4,11 @@ public class PriceCalculator {
     private final double maxShippingCost = 100.0;
 
     double getPrice(int quantity, int itemPrice) {
-
         double basePrice = quantity * itemPrice;
-        double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+
+        double discountedQuantity = Math.max(0, quantity - 500);
+        double discount = discountedQuantity * itemPrice * 0.05;
+        
         double shippingCost=Math.min(basePrice * 0.1, maxShippingCost);
 
         return basePrice - discount + shippingCost;
