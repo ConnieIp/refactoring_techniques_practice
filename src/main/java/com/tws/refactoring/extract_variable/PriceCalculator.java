4ca76980973a9d9ff67953134a457b05ca517c8f
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 22:53:03 +0800

    refactoring extract maxShippingCost variable

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,12 +1,14 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private final double maxShippingCost = 100.0;
+
     double getPrice(int quantity, int itemPrice) {
+
         double basePrice = quantity * itemPrice;
         double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
-        double shippingCost=Math.min(basePrice * 0.1, 100.0);
-        return basePrice -
-                discount +
-                shippingCost;
+        double shippingCost=Math.min(basePrice * 0.1, maxShippingCost);
+
+        return basePrice - discount + shippingCost;
     }
 }
