302e851266c55c980bff88ea35dbe58a6e6eae31
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 23:04:49 +0800

    refactoring extract maxDiscountQuantity field

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -4,11 +4,12 @@ public class PriceCalculator {
     private final double maxShippingCost = 100.0;
     private final double shippingCostRate = 0.1;
     private final double discountRate = 0.05;
+    private final double maxDiscountQuantity = 500;
 
     double getPrice(int quantity, int itemPrice) {
         double basePrice = quantity * itemPrice;
 
-        double discountedQuantity = Math.max(0, quantity - 500);
+        double discountedQuantity = Math.max(0, quantity - maxDiscountQuantity);
         double discount = discountedQuantity * itemPrice * discountRate;
 
         double shippingCost=Math.min(basePrice * shippingCostRate, maxShippingCost);
