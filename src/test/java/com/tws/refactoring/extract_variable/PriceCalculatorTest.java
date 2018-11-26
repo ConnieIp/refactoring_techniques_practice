48c94002068a5765aa339d628de74276c6c2d02f
Connie Ip <connie.ip@cargosmart.com>
2018-11-26 22:47:22 +0800

    add testcase for PriceCalculator

--- src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -1,5 +1,21 @@
 package com.tws.refactoring.extract_variable;
 
-public class PriceCalculatorTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class PriceCalculatorTest {
+    @Test
+    public void test_get_price(){
+        PriceCalculator priceCalculator=new PriceCalculator();
+        double result=priceCalculator.getPrice(3,5);
+        assertEquals(16.5,result,0.001);
+    }
+
+    @Test
+    public void test_get_price_quantity_larger_than_500(){
+        PriceCalculator priceCalculator=new PriceCalculator();
+        double result=priceCalculator.getPrice(600,10);
+        assertEquals(6050,result,0.001);
+    }
 }
